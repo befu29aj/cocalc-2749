5d802415ec5fa03b20555590928e350d322a892d
Stefan Lukits <sediomyle@streetgreek.com>
2017-11-23 20:10:55 -0800

    i

--- auto/hm-07-skepticism.el
+++ auto/hm-07-skepticism.el
@@ -3,15 +3,6 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "xcolor=dvipsnames")))
-   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
@@ -21,6 +12,5 @@
    (TeX-add-symbols
     "CourseName"
     "CourseNumber"
-    "CourseInst"))
- :latex)
+    "CourseInst")))
 
